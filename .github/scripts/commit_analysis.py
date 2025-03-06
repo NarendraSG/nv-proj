1fd37e6b4ee391138a7b9627829dfa06580633bd
NarendraSG <narendra@studiographene.com>
2025-03-06 09:26:26 +0530

    afdgsbd

--- .github/scripts/commit_analysis.py
+++ .github/scripts/commit_analysis.py
@@ -9,6 +9,7 @@ DEBUG = True
 # Define the 30-day threshold
 # THIRTY_DAYS = timedelta(days=30)
 # THIRTY_DAYS = timedelta(seconds=30)
+# THIRTY_DAYS = timedelta(seconds=100)
 THIRTY_DAYS = timedelta(seconds=100)
 
 def debug_log(message):
