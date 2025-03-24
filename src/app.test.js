bc2bf715d7a7dc1e3cdf42e8e558133befb1fad9
NarendraSG <narendra@studiographene.com>
2025-03-24 12:33:02 +0530

    c1: rewrite=5, newfeature=3

--- src/app.test.js
+++ src/app.test.js
@@ -2,20 +2,24 @@ describe('ClientController (e2e)', () => {
 
 
 	it('/client (POST) create client123', async () => {
-		expect(3).toBe(3);
-    });
+		// expect(3).toBe(3);
+    // });
+
+    // it('/client (POST) create client', async () => {
+		// expect(5).toBe(5);
+    // });
+
 
     it('/client (POST) create client', async () => {
-		expect(5).toBe(5);
+		expect(2).toBe(2);
     });
-
 
     it('/client (POST) create client', async () => {
 		expect(2).toBe(2);
     });
 
     it('/client (POST) create client', async () => {
-		expect(2).toBe(2);
+      expect(8).toBe(8);
     });
 
     it('/client (POST) create client', async () => {
